65d6a2487d3b3cb3576c049129f87e7fa8a85f43
ozx <359835703@qq.com>
2018-12-11 21:50:08 +0800

    添加模板

---
+++ front_end_pc/js/template.js
@@ -0,0 +1,40 @@
+$(function (){
+
+    $("#homepage").attr("href", "./meetingHomePage.html?meetingId=" + loadPageVar("meetingId") );
+
+    $.ajax({
+        url: globalUrl.globalUrl + "modules/"+ loadPageVar("moduleId") + "/meetings/" + loadPageVar("meetingId"),
+        method: "GET",
+        success: function ( data ) {
+            /*
+            * content: "<p>请输入内容</p><p>ddddd</p>"
+              errmsg: "请求成功"
+              "title": ""
+              errno: "0"
+            * */
+            if( data["errno"] !== "0" ){
+                 alert( data["errmsg"] );
+                 return;
+            }
+
+            let temp = '<a href="" style="color: #fff;">'+ data["title"] +'</a>\n';
+            $(".header_title").append( temp );
+
+            $("#section").html( data["content"] );
+
+        },
+        error: function ( err ) {
+            console.log( err );
+        }
+    })
+
+
+    //获取URL参数
+    /*
+    * param1 { String } 参数名
+    * */
+    function loadPageVar( sVar ) {
+        return decodeURI( window.location.search.replace( new RegExp( "^(?:.*[&\\?]" + encodeURI( sVar ).replace( /[\.\+\*]/g, "\\$&" ) + "(?:\\=([^&]*))?)?.*$", "i" ), "$1" ) );
+    }
+
+})
